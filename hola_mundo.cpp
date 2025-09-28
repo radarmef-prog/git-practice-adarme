4e696e35df04eefe037ec31346f559f6dc9e989f
RubenAdarme <radarmef@gmail.com>
2025-09-28 10:54:18 -0400

    feat: crear programa hola mundo inicial

---
+++ hola_mundo.cpp
@@ -0,0 +1,9 @@
+// hola_mundo.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es Rubén Adarme" << endl;
+    return 0;
+}
